eeebf878926c6afba1126b4f50ae6ac96d959df7
tkyko13 <>
2020-05-04 12:15:48 +0900

    upd

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello ProtoOut!");
+// axiosライブラリを呼び出す
+const axios = require("axios");
+
+// 実際にデータを取得する getRequest 関数
+async function getRequest() {
+  try {
+    const response = await axios.get(
+      `http://weather.livedoor.com/forecast/webservice/json/v1?city=130010`
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getRequest を呼び出してデータを読み込む
+getRequest();
